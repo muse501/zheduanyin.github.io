ef26f24cfffc4dba0bef1d08b58ffac5dc441933
zheduanyin <zheduanyin@163.com>
2016-03-18 12:13:17 +0800

    Site updated: 2016-03-18 12:13:16

--- js/scroll_to_animation.js
+++ js/scroll_to_animation.js
@@ -1,10 +1,1 @@
-var $body = $("html, body");
-$("#toc a").click(function() {
-    $body.animate({
-        scrollTop: $($(this).attr("href")).offset().top + "px"
-    }, {
-        duration: 500,
-        easing: "swing"
-    });
-    return false;
-});
+var $body=$("html, body");$("#toc a").click(function(){$body.animate({scrollTop:$($(this).attr("href")).offset().top+"px"},{duration:500,easing:"swing"});return false});
