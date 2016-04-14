a652a141fe6baedee9b3ab070579a6aa7f0f0d7d
zheduanyin <zheduanyin@163.com>
2016-04-14 15:22:14 +0800

    Site updated: 2016-04-14 15:22:12

--- js/toc.js
+++
@@ -1,1 +0,0 @@
-var $toc=$("#toc");var tocInitOffsetTop=$toc.offset()?$toc.offset().top:0;var tocInitWidth=$toc.outerWidth();$(window).scroll(function(){if($(window).scrollTop()>tocInitOffsetTop-80){$toc.addClass("fixed-toc").css("width",tocInitWidth+"px")}else{$toc.removeClass("fixed-toc")}});
